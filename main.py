05f2bd4add7aeb354764eec6d131f02d10565869
Mitchell Roger <roger.mitch27@gmail.com>
2022-07-23 15:57:51 +1000

    add lives counter

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
-
-
 import random
 
 
-random_generator=random.randint(0, 10)
-
+random_generator = random.randint(1, 10)
 print(random_generator)
+
+
 lives = 5
-game_on=True
-while game_on == True:
-    
+game_on = True
+
+while game_on:
+
     guess = input("choose a number between 1 and 10 ")
+
     if lives == 0:
-        print("you ran out of lives")
-        game_on =False
-        play_game=input("play again y/n")   
+        print(f"bad luck you have {lives} lives ")
+        game_on = False
+        play_game = input("play again y/n")
+        if play_game == "y":
+            game_on = True
+            random_generator = random.randint(1, 10)
+        else:
+            break
     elif int(guess) > random_generator:
         print("To high")
+        print(f"you have {lives} lives left")
         lives -= 1
     elif int(guess) < random_generator:
         print("To low")
         lives -= 1
-    else: 
+        print(f"you have {lives} lives left")
+    else:
         print("you guessed correct")
-        print(f"lives left{lives}")
+
         game_on = False
-        play_game=input("play again y/n")
-        
+        play_game = input("play again y/n ")
+
         if play_game == "y":
-             game_on = True
-           
-            
-        
-        
+            game_on = True
+            random_generator = random.randint(0, 10)
+        else:
+            break
+
 
