40907e9b425da8546e6b8d98b081b0fb0fd86624
Mitchell Roger <roger.mitch27@gmail.com>
2022-07-23 15:56:13 +1000

    add live's logic

--- main.py
+++ main.py
@@ -6,17 +6,31 @@ import random
 random_generator=random.randint(0, 10)
 
 print(random_generator)
+lives = 5
 game_on=True
 while game_on == True:
     
     guess = input("choose a number between 1 and 10 ")
-
-    if int(guess) > random_generator:
+    if lives == 0:
+        print("you ran out of lives")
+        game_on =False
+        play_game=input("play again y/n")   
+    elif int(guess) > random_generator:
         print("To high")
+        lives -= 1
     elif int(guess) < random_generator:
         print("To low")
+        lives -= 1
     else: 
         print("you guessed correct")
+        print(f"lives left{lives}")
         game_on = False
+        play_game=input("play again y/n")
+        
+        if play_game == "y":
+             game_on = True
+           
+            
+        
         
 
